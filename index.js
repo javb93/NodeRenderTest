fdc1a0b68e03aa5a9b4221d58ea6ccfd20222e45
Alfredo Vidal <39306610+javb93@users.noreply.github.com>
2024-07-29 19:57:17 -0600

    Adding mysql

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const mysql = require("mysql");
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "root",
+  database: process.env.DB_NAME || "test",
+});
 
-const express = require('express')
-const app = express()
-const port =process.env.PORT || 3000
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/test', (req, res) => {
-  res.send('NOW I ANSWER THIS OTHER THING!')
-})
-app.post('/user', (req, res) => {
-  res.send('Got a POST request with this body: ' + JSON.stringify(req.body))
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/users", (req, res) => {
+  conn.query("SELECT * FROM users", function (err, result) {
+    if (err) throw
+    res.send(result);
+  });
+});
+app.post("/user", (req, res) => {
+  res.send("Got a POST request with this body: " + JSON.stringify(req.body));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
